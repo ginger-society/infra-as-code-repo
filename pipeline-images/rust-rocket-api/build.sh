0fc76abbada313bd4160cecddf3d67bf03ddad4f
Ginger Society Admin <admin@gingersociety.org>
2024-08-20 20:12:42 +0530

    feat: added docker file for builder image for react - vite SPA

--- pipeline-images/rust-rocket-api/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build . -t gingersociety/rust-rocket-api-builder -f Dockerfile.builder --platform=linux/amd64
-
-docker push gingersociety/rust-rocket-api-builder
-
-docker build . -t gingersociety/rust-rocket-api-runner -f Dockerfile.runner --platform=linux/amd64
-
-docker push gingersociety/rust-rocket-api-runner
